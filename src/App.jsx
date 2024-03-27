0808d0559b9bc762953cf3dfa899a52793fa3a83
urooj khan <urooj.khan@lelafe.com>
2024-03-28 04:53:39 +0530

    Added Player component to App component Modified Player component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import Player from "./components/Player";
+import Player from "./components/Player.jsx";
 
 function App() {
   return (
     <main>
       <div id="game-container">
         <ol id="players">
-          <Player name="Max" symbol="X"></Player>
-          <Player name="Manuel" symbol="0"></Player>
+          <Player name="Max" symbol="X"/>
+          <Player name="Manuel" symbol="0"/>
         </ol>
       </div>
     </main>
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,18 @@
-export default function Player({name, symbol}) {
+import { useState } from "react";
+
+export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+  function handleEditClick() {
+    setIsEditing((isEditing) => !isEditing);
+  }
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {!isEditing && <span className="player-name">{name}</span>}
+        {isEditing && <input type="text" value={name} required />}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 }
