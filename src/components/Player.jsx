a6d0ccaffa436ca9fd9568073e1a328aa24026ee
urooj khan <urooj.khan@lelafe.com>
2024-03-28 12:40:47 +0530

    Modified Player component: Added two-way data binding to input

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,15 +1,26 @@
 import { useState } from "react";
 
 export default function Player({ name, symbol }) {
+  const [playerName, setPlayerName] = useState(name);
   const [isEditing, setIsEditing] = useState(false);
   function handleEditClick() {
     setIsEditing((isEditing) => !isEditing);
   }
+  function handleNameChange(event) {
+    setPlayerName(event.target.value);
+  }
   return (
     <li>
       <span className="player">
-        {!isEditing && <span className="player-name">{name}</span>}
-        {isEditing && <input type="text" value={name} required />}
+        {!isEditing && <span className="player-name">{playerName}</span>}
+        {isEditing && (
+          <input
+            type="text"
+            value={playerName}
+            required
+            onChange={handleNameChange}
+          />
+        )}
         <span className="player-symbol">{symbol}</span>
       </span>
       <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
