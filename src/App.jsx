2ec76b523de692d95e8798171adb89bc726779c3
urooj khan <khanurooj5299@gmail.com>
2024-03-29 14:06:22 +0530

    Added an activePlayer state to parent App component which was needed in both GAmeboard and Player component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import Player from "./components/Player.jsx";
 import GameBoard from "./components/GameBoard.jsx";
+import { useState } from "react";
 
 function App() {
+  const [activePlayer, setActivePlayer] = useState('X');
+  function handlePlayerChange() {
+    setActivePlayer(activePlayer=>activePlayer=="X"?'0':"X");
+  }
+
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player name="Max" symbol="X"/>
-          <Player name="Manuel" symbol="0"/>
+        <ol id="players" className="highlight-player">
+          <Player name="Max" symbol="X" isActive={activePlayer=="X"}/>
+          <Player name="Manuel" symbol="0" isActive={activePlayer=="0"}/>
         </ol>
-        <GameBoard/>
+        <GameBoard handlePlayerChange={handlePlayerChange} activePlayer={activePlayer}/>
       </div>
     </main>
   );
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -6,15 +6,16 @@ const initialGameBoard = [
   [null, null, null],
 ];
 
-export default function GameBoard() {
+export default function GameBoard({handlePlayerChange, activePlayer}) {
     const [gameBoard, setGameBoard]=useState(initialGameBoard);
 
     function handleSelectSquare(row, col) {
-        setGameBoard(gameBoard => {
-            const newGameBoard = [...gameBoard.map(row => [...row])];
-            newGameBoard[row][col]='X';
-            return newGameBoard;
-        });
+      setGameBoard(gameBoard => {
+          const newGameBoard = [...gameBoard.map(row => [...row])];
+          newGameBoard[row][col]=activePlayer;
+          return newGameBoard;
+      });
+      handlePlayerChange();
     }
   return (
     <ol id="game-board">
