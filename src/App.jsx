17d465bc834ff40b05a527724e7e3afe299e9b0a
urooj khan <khanurooj5299@gmail.com>
2024-04-24 11:41:06 +0530

    Refactored App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,13 @@ import { useState } from "react";
 import Player from "./components/Player.jsx";
 import GameBoard from "./components/GameBoard.jsx";
 import Log from "./components/Log.jsx";
-import GameOver from "./components/GameOver.jsx"
+import GameOver from "./components/GameOver.jsx";
+const PLAYERS = {
+  'X': 'Player 1',
+  '0': 'Player 2',
+};
 
-const initialGameBoard = [
+const INITIAL_GAME_BOARD = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
@@ -15,68 +19,69 @@ function deriveActivePlayer(gameTurns) {
   return gameTurns.length ? (gameTurns[0].symbol == "X" ? "0" : "X") : "X";
 }
 
-function isWinner(gameBoard, lastTurn, playerNames) {
-  let rowCombinationFound = true;
-  let colCombinationFound = true;
-  let diagCombinationFound = true;
-  //checking the row
-  for (let col = 0; col <= 2; col++) {
-    if (gameBoard[lastTurn.rowIndex][col] !== lastTurn.symbol) {
-      rowCombinationFound = false;
-      break;
-    }
-  }
-  if (rowCombinationFound) return playerNames[lastTurn.symbol];
-  //checking the column
-  for (let row = 0; row <= 2; row++) {
-    if (gameBoard[row][lastTurn.colIndex] !== lastTurn.symbol) {
-      colCombinationFound = false;
-      break;
-    }
-  }
-  if (colCombinationFound) return playerNames[lastTurn.symbol];
-  //checking the main diagonal
-  if (lastTurn.rowIndex == lastTurn.colIndex) {
-    for (let row = 0, col = 0; row <= 2; row++, col++) {
-      if (gameBoard[row][col] !== lastTurn.symbol) {
-        diagCombinationFound = false;
+function deriveWinner(gameBoard, lastTurn, playerNames) {
+  //only if some player has played
+  if (lastTurn) {
+    let rowCombinationFound = true;
+    let colCombinationFound = true;
+    let diagCombinationFound = true;
+    //checking the row
+    for (let col = 0; col <= 2; col++) {
+      if (gameBoard[lastTurn.rowIndex][col] !== lastTurn.symbol) {
+        rowCombinationFound = false;
         break;
       }
     }
-    if (diagCombinationFound) return playerNames[lastTurn.symbol];
-  }
-  //reset
-  diagCombinationFound = true;
-  //checking for cross diagonal
-  if (lastTurn.rowIndex + lastTurn.colIndex == 2) {
-    for (let row = 0, col = 2; row <= 2; row++, col--) {
-      if (gameBoard[row][col] !== lastTurn.symbol) {
-        diagCombinationFound = false;
+    if (rowCombinationFound) return playerNames[lastTurn.symbol];
+    //checking the column
+    for (let row = 0; row <= 2; row++) {
+      if (gameBoard[row][lastTurn.colIndex] !== lastTurn.symbol) {
+        colCombinationFound = false;
         break;
       }
     }
-    if(diagCombinationFound) return playerNames[lastTurn.symbol];
+    if (colCombinationFound) return playerNames[lastTurn.symbol];
+    //checking the main diagonal
+    if (lastTurn.rowIndex == lastTurn.colIndex) {
+      for (let row = 0, col = 0; row <= 2; row++, col++) {
+        if (gameBoard[row][col] !== lastTurn.symbol) {
+          diagCombinationFound = false;
+          break;
+        }
+      }
+      if (diagCombinationFound) return playerNames[lastTurn.symbol];
+    }
+    //reset
+    diagCombinationFound = true;
+    //checking for cross diagonal
+    if (lastTurn.rowIndex + lastTurn.colIndex == 2) {
+      for (let row = 0, col = 2; row <= 2; row++, col--) {
+        if (gameBoard[row][col] !== lastTurn.symbol) {
+          diagCombinationFound = false;
+          break;
+        }
+      }
+      if (diagCombinationFound) return playerNames[lastTurn.symbol];
+    }
+    return false;
+  } else return false;
+}
+
+function deriveGameBoard(gameTurns) {
+  const gameBoard = structuredClone(INITIAL_GAME_BOARD);
+  for (const turn of gameTurns) {
+    gameBoard[turn.rowIndex][turn.colIndex] = turn.symbol;
   }
-  return false;
+  return gameBoard;
 }
 
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-  const [playerNames, setPlayerNames] = useState({
-    'X': 'Player 1',
-    '0': 'Player 2'
-  });
+  const [playerNames, setPlayerNames] = useState(PLAYERS);
   const activePlayer = deriveActivePlayer(gameTurns);
-  const gameBoard = structuredClone(initialGameBoard);
-  for (const turn of gameTurns) {
-    gameBoard[turn.rowIndex][turn.colIndex] = turn.symbol;
-  }
-  let winner;
-  if (gameTurns.length) {
-    winner = isWinner(gameBoard, gameTurns[0], playerNames);
-  }
-
-  const hasDraw = gameTurns.length ==9 && !winner;
+  const gameBoard = deriveGameBoard(gameTurns);
+  const winner = deriveWinner(gameBoard, gameTurns[0], playerNames);
+  const hasDraw = gameTurns.length == 9 && !winner;
 
   function handlePlayerChange(rowIndex, colIndex) {
     setGameTurns((prevTurns) => {
@@ -98,18 +103,31 @@ function App() {
   }
 
   function handlePlayerNameChange(symbol, newName) {
-    setPlayerNames((playerNames)=>{
-      return {...playerNames, [symbol]: newName};
+    setPlayerNames((playerNames) => {
+      return { ...playerNames, [symbol]: newName };
     });
   }
+
   return (
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player name={playerNames['X']} symbol="X" isActive={activePlayer == "X"} handlePlayerNameChange={handlePlayerNameChange}/>
-          <Player name={playerNames['0']} symbol="0" isActive={activePlayer == "0"} handlePlayerNameChange={handlePlayerNameChange}/>
+          <Player
+            name={playerNames["X"]}
+            symbol="X"
+            isActive={activePlayer == "X"}
+            handlePlayerNameChange={handlePlayerNameChange}
+          />
+          <Player
+            name={playerNames["0"]}
+            symbol="0"
+            isActive={activePlayer == "0"}
+            handlePlayerNameChange={handlePlayerNameChange}
+          />
         </ol>
-        {(winner || hasDraw) && <GameOver winner={winner} handleRestart={handleRestart}/>}
+        {(winner || hasDraw) && (
+          <GameOver winner={winner} handleRestart={handleRestart} />
+        )}
         <GameBoard
           handlePlayerChange={handlePlayerChange}
           gameBoard={gameBoard}
